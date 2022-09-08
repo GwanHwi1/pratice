20cab3e9e186db9df3b89f5bd71bdcade9cb0a32
GwanHwi1 <ghc98@naver.com>
2022-09-08 14:54:40 +0900

    상하좌우

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
-n= int(input())
-count1=0
-count2=0
-count3=0
-for i in range(60): #60초
-  if i<10:
-    if i==3: count1+=1
-  if i>=10:
-    if str(i)[0]=='3' or str(i)[1]=='3': count1+=1
-for i in range(60): #60분
-  if i<10:
-    if i==3: count2+=60
-    else: count2+=count1
-  if i>=10:
-    if str(i)[0]=='3' or str(i)[1]=='3': count2+=60
-    else: count2+=count1
-for i in range(n+1): #n시간
-  if i < 10:
-    if i==3: count3+=3600
-    else: count3+=count2
-  if i >=10:
-    if str(i)[0]=='3' or str(i)[1]=='3': count3+=3600
-    else: count3+=count2
-print(count3)
-
-  
-  
+x=1
+y=1
+point={'U':-1, 'D':1, 'L':-1, 'R':1}
+point_key=list(point.keys())
+n=int(input())
+a=list(map(str, input().split()))
+count1=count2=count3=count4=0
+remove_set={0}
+for i in range(len(a)):
+  if a[i]=='U': count1+=1
+  elif a[i]=='D': count2+=1
+  elif a[i]=='L': count3+=1
+  else : count4+=1
+  if count1>count2 :
+    count1-=1
+    a[i] = 0
+  elif count3>count4 :
+    count3-=1
+    a[i] = 0
+result = [i for i in a if i not in remove_set]
+for i in range(n):
+  if result[i] == point_key[0] or result[i] == point_key[1] :
+    x+=point[result[i]]
+    if x<=0: x=1
+  else:
+    y+=point[result[i]]
+    if y<=0: y=1
+print(x,y)
