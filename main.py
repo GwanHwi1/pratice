51f8ec8e93c1190b7e3c100cab95e3a9668f845c
GwanHwi1 <ghc98@naver.com>
2022-09-10 00:22:46 +0900

    문자열 압축

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-data=input()
-a=[]
-totalstr=""
-total=0
-for i in range(len(data)):
-  if ord('A')<=ord(data[i])<=ord('Z'):
-    a.append(data[i])
-  else: total+=int(data[i]) 
-a.sort()
-for i in a:
-  totalstr+=i
-print(totalstr+str(total))
+s= input()
+def solution(s):
+  result=len(s)
+  if len(s)==1: result=1
+  for i in range(1,len(s)//2+1):
+    arr=[]
+    a=0
+    while(a<len(s)):
+      if len(s)-a<i: 
+        arr.append(s[a:])
+        a+=i
+      else: 
+        arr.append(s[a:a+i])
+        a+=i
+    count=1
+    b=[]
+    for j in range(len(arr)):
+      if j==len(arr)-1:
+        if count != 1: b.append(str(count))
+        b.append(arr[j])
+        break
+      if arr[j]==arr[j+1]: count+=1
+      else:
+        if count!=1:
+          b.append(str(count))
+          count=1
+        b.append(arr[j])
+    leng=(''.join(b))
+    result=min(result,len(leng))
+  return result
--- practice.py
+++ practice.py
@@ -1,15 +1,16 @@
-data = input()
-result=[]
-value=0
-
-for x in data:
-  if x.isalpha():
-    result.append(x)
-  else: value+=int(x)
-
-result.sort()
-
-if value!=0:
-  result.append(str(value))
-
-print(''.join(result))
+def solution(s):
+  answer = len(s)
+  for step in range (1, len(s)//2+1):
+    compressed=""
+    prev = s[0:step]
+    count =1
+    for j in range(step, len(s), step):
+      if prev == s[j:j+step]:
+        count +=1
+      else:
+        compressed += str(count) + prev if count >= 2 else prev
+        prev = s[j:j+step]
+        count =1
+    compressed += str(count) + prev if count>= 2 else prev
+    answer = min(answer,len(compressed))
+  return answer
