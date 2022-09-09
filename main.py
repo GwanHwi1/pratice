939245ecaf18fd2626e16f5451b63cfac88e96eb
GwanHwi1 <ghc98@naver.com>
2022-09-09 16:10:40 +0900

    문자열 재정렬

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-a= input()
-count=0
-point=['a','b','c','d','e','f','g','h']
-x,y=0,0
-
-for i in range(len(point)): #입력값 좌표설정
-  if a[0]==point[i]: x=i+1
-y=int(a[1])
-
-if x-2>0:
-  if y+1<9: count+=1
-  if y-1>0: count+=1
-if x+2<9:
-  if y+1<9: count+=1
-  if y-1>0: count+=1
-if y-2>0:
-  if x+1<9: count+=1
-  if x-1>0: count+=1
-if y+2<9:
-  if x+1<9: count+=1
-  if x-1>0: count+=1
-print(count)
+data=input()
+a=[]
+totalstr=""
+total=0
+for i in range(len(data)):
+  if ord('A')<=ord(data[i])<=ord('Z'):
+    a.append(data[i])
+  else: total+=int(data[i]) 
+a.sort()
+for i in a:
+  totalstr+=i
+print(totalstr+str(total))
