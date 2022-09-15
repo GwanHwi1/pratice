6ef498ffdef661cce80c7cdcd09268285ce6a4b3
GwanHwi1 <ghc98@naver.com>
2022-09-15 17:10:40 +0900

    파스칼의 삼각형

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 t=int(input())
 for i in range(1,t+1):
-  a=input()
-  part=0
-  result=0
-  for j in range(1,11):
-    arr=[]
-    for k in range(0,len(a)-j,j):
-      arr.append(a[k:k+j])
-    for k in range(len(arr)):
-      if k==len(arr)-1: break
-      else:
-        if arr[k]==arr[k+1]: result=1
-        else:
-          result=0
-          break
-    if result==1: 
-      part=j
-      break
-  print('#'+str(i),part)
+  n=int(input())
+  arr=[]
+  string=""
+  for j in range(n):
+    arr1=[]
+    for k in range(n):
+      if k==0:
+        arr1.append(1)
+        string+=str(1)+" "
+      elif k<=j:
+        arr1.append(arr[j-1][k-1]+arr[j-1][k])
+        string+=str(arr[j-1][k-1]+arr[j-1][k])+" "
+      else: arr1.append(0)
+    string+="\n"  
+    arr.append(arr1)
+  print("#"+str(i))
+  print(string[:len(string)-1])
--- practice.py
+++ practice.py
@@ -1,8 +1,14 @@
-n=int(input())
-for i in range(1,n+1):
-  count=0
-  for j in str(i):
-    if j =='3' or j=='6' or j=='9': count+=1
-  if count!=0:
-    print('-'*count,end=" ")
-  else: print(i,end=" ")
+t = int(input())
+for tc in range(t):
+    n = int(input())
+ 
+    pascal = [[1]*i for i in range(1, n+1)]
+    # print(pascal)
+    for k in range(2, n):
+        for g in range(1, k):
+            pascal[k][g] = pascal[k-1][g-1]+pascal[k-1][g]
+ 
+    # print(pascal)
+    print(f'#{tc+1}')
+    for j in pascal:
+        print(*j)
