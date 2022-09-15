1031e3b5d69d5ef529beeea29e27e834e0c3de24
GwanHwi1 <ghc98@naver.com>
2022-09-15 15:47:59 +0900

    패턴 마디의 길이

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 t=int(input())
 for i in range(1,t+1):
-  n=int(input())
-  a=list(map(int,input().split()))
-  profit=0
-  maxValue=max(a)
-  for j in range(len(a)-1):
-    if a[j]<maxValue: profit+=maxValue-a[j]
-    else: maxValue=max(a[j+1:])
-  print("#"+str(i),profit)
-      
+  a=input()
+  part=0
+  result=0
+  for j in range(1,11):
+    arr=[]
+    for k in range(0,len(a)-j,j):
+      arr.append(a[k:k+j])
+    for k in range(len(arr)):
+      if k==len(arr)-1: break
+      else:
+        if arr[k]==arr[k+1]: result=1
+        else:
+          result=0
+          break
+    if result==1: 
+      part=j
+      break
+  print('#'+str(i),part)
