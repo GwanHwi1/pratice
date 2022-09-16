b6b37dc30c5f1dadbd8b72c969f7c9e36ea934c6
GwanHwi1 <ghc98@naver.com>
2022-09-16 17:02:43 +0900

    지그재그 숫자

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 t=int(input())
 for i in range(1,t+1):
-  a=input()
-  b=""
-  result=0
-  for j in range(len(a)-1,-1,-1):
-    b+=a[j]
-  if a==b: result=1
-  print("#"+str(i),result)
+  n=int(input())
+  total=0
+  for j in range(1,n+1):
+    if j%2==0: total-=j
+    else: total+=j
+  print("#"+str(i),total)
