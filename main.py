c62a4feca24dc37aa2a57f7b73ebbc2a1b8e94a3
GwanHwi1 <ghc98@naver.com>
2022-09-16 15:36:04 +0900

    파리 퇴치

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 t=int(input())
 for i in range(1,t+1):
-  n=int(input())
-  arr=[]
-  string=""
-  for j in range(n):
-    arr1=[]
-    for k in range(n):
-      if k==0:
-        arr1.append(1)
-        string+=str(1)+" "
-      elif k<=j:
-        arr1.append(arr[j-1][k-1]+arr[j-1][k])
-        string+=str(arr[j-1][k-1]+arr[j-1][k])+" "
-      else: arr1.append(0)
-    string+="\n"  
-    arr.append(arr1)
-  print("#"+str(i))
-  print(string[:len(string)-1])
+  n,m=map(int, input().split())
+  arr=[list(map(int,input().split()))for _ in range(n)]
+  maxValue=0
+  total=0
+  for j in range(n-m+1):#세로
+    for k in range(n-m+1):#가로
+      for l in range(m):
+        total+=sum(arr[j+l][k:k+m])
+      maxValue=max(maxValue,total)
+      total=0
+  print("#"+str(i),maxValue)
--- practice.py
+++ practice.py
@@ -1,14 +1,2 @@
-t = int(input())
-for tc in range(t):
-    n = int(input())
- 
-    pascal = [[1]*i for i in range(1, n+1)]
-    # print(pascal)
-    for k in range(2, n):
-        for g in range(1, k):
-            pascal[k][g] = pascal[k-1][g-1]+pascal[k-1][g]
- 
-    # print(pascal)
-    print(f'#{tc+1}')
-    for j in pascal:
-        print(*j)
+a=[1,2,3,4,5]
+print(sum(a[3:5]))
