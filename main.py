05da6ecddde6fd077fda514350a4347328cb0758
GwanHwi1 <ghc98@naver.com>
2022-09-16 18:00:15 +0900

    어디에 단어가 들어갈 수 있을까

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 t=int(input())
 for i in range(1,t+1):
-  n=int(input())
-  total=0
-  for j in range(1,n+1):
-    if j%2==0: total-=j
-    else: total+=j
-  print("#"+str(i),total)
+  n,k=map(int, input().split())
+  puzzel=[list(map(int,input().split()))for _ in range(n)]
+  result=0
+  for j in range(n):
+    count=1
+    for l in range(n):
+      if l==n-1:
+        if count==k: result+=1 
+      else:
+        if puzzel[j][l]==puzzel[j][l+1]==1: count+=1
+        else: 
+          if count==k: 
+            result+=1
+            count=1
+          else: count=1
+  for j in range(n):
+    count=1
+    for l in range(n):
+      if l==n-1:
+        if count==k: result+=1 
+      else:
+        if puzzel[l][j]==puzzel[l+1][j]==1: count+=1
+        else: 
+          if count==k: 
+            result+=1
+            count=1
+          else: count=1
+  print("#"+str(i),result)
