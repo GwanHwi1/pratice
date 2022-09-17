f10627a31ff7be4ec46eceaa08b53640a194cd29
GwanHwi1 <ghc98@naver.com>
2022-09-18 00:26:32 +0900

    쉬운 거스름돈

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
 t=int(input())
 for i in range(1,t+1):
-  puzzel=[list(map(int, input().split()))for _ in range(9)]
-  result=1
-  for j in range(9): #가로 1~9 확인
-    arr=set(puzzel[j])
-    if len(arr) !=9:
-      result=0
-      break
-  for j in range(9): #세로 1~9 확인
-    arr=set()
-    for k in range(9):
-      arr.add(puzzel[k][j])      
-    if len(arr)!=9: 
-      result=0
-      break
-  for j in range(0,9,3): #3*3 1~9 확인
-    for k in range(0,9,3):
-      arr=set()
-      for l in range(3):
-        arr.update(puzzel[j+l][k:k+3])
-      if len(arr)!=9:
-        result=0
-        break
-  print("#"+str(i),result) 
+  money=int(input())
+  count=0
+  arr=[50000,10000,5000,1000,500,100,50,10]
+  balance=[]
+  for j in range(len(arr)):
+    cal=money//arr[j]
+    balance.append(cal)
+    if cal>0: money-=arr[j]*cal
+  print("#"+str(i))
+  print(*balance)
