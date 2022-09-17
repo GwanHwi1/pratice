c720419efe520a5d25588411043cc5a2a339ccbd
GwanHwi1 <ghc98@naver.com>
2022-09-17 22:54:29 +0900

    시각 덧셈

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
 t=int(input())
 for i in range(1,t+1):
-  n,k=map(int, input().split())
-  puzzel=[list(map(int,input().split()))for _ in range(n)]
-  result=0
-  for j in range(n):
-    count=1
-    for l in range(n):
-      if l==n-1:
-        if count==k: result+=1 
-      else:
-        if puzzel[j][l]==puzzel[j][l+1]==1: count+=1
-        else: 
-          if count==k: 
-            result+=1
-            count=1
-          else: count=1
-  for j in range(n):
-    count=1
-    for l in range(n):
-      if l==n-1:
-        if count==k: result+=1 
-      else:
-        if puzzel[l][j]==puzzel[l+1][j]==1: count+=1
-        else: 
-          if count==k: 
-            result+=1
-            count=1
-          else: count=1
-  print("#"+str(i),result)
+  a,b,c,d=map(int,input().split())
+  hour=0
+  minute=0
+  minute=(b+d)%60
+  if b+d>59: hour=(a+c+1)%12
+  else: hour=(a+c)%12
+  if hour==0: hour=12
+  print("#"+str(i),hour,minute)
