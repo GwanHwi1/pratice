e386d84357b78636d830a0d6088e486fb1033cbf
GwanHwi1 <ghc98@naver.com>
2022-10-08 18:35:18 +0900

    분해합

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from itertools import combinations
-
-n,m=map(int,input().split())
-nums=list(map(int,input().split()))
-data=list(combinations(nums,3))
-result=0
-for i in data:
-  a=sum(i)
-  if a<=m:
-    result=max(result,a)
-
-print(result)
+n=int(input())
+for i in range(n):
+  result=0
+  a=str(i)
+  for j in a:
+    result+=int(j)
+  result+=i
+  if result==n:
+    print(i)
+    break
+  elif result!=n and i==n-1:print(0)
