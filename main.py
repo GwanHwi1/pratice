d5ce00179600c3e6e548be87da62dd4c2a27c48b
GwanHwi1 <ghc98@naver.com>
2022-10-10 21:36:35 +0900

    덩치

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-def hanoi(n,a,b):
-  if n==1:
-    print(a,b)
-    return
-  hanoi(n-1,a,6-a-b)
-  print(a,b)
-  hanoi(n-1, 6-a-b,b)
+def bigger(arr,result,n):
+  if n>=len(arr): return result
+  for i in range(len(arr)):
+    if i!=n:
+      if arr[n][0] > arr[i][0] and arr[n][1] > arr[i][1]:
+        result[n][0]+=1
+      elif arr[n][0] >=arr[i][0] and arr[n][1] <= arr[i][1]:
+        result[n][1]+=1
+      elif arr[n][0] <= arr[i][0] and arr[n][1] >= arr[i][1]:
+        result[n][1]+=1
+  bigger(arr,result,n+1)
 
 n=int(input())
-print(2**n-1)
-hanoi(n,1,3)
+result=[[0,0]for _ in range(n)]
+lst=[list(map(int, input().split()))for _ in range(n)]
+bigger(lst,result,0)
+result1=[]
+for i in range(len(result)):
+  result1.append(n-sum(result[i]))
+print(' '.join(str(e) for e in result1))
+
