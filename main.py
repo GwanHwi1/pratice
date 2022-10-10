ec503f8dc6e34cd9c6fd66fa87559e77e7210538
GwanHwi1 <ghc98@naver.com>
2022-10-10 16:06:13 +0900

    별찍기-10

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 n=int(input())
+arr = [[" "for _ in range(n)]for _ in range(n)]
+
+def sol(x,y,n):
+  if n==1:
+    arr[x][y]="*"
+    return
+  for i in range(3):
+    for j in range(3):
+      if (i==1 and j==1): continue
+      else: sol(x+i*(n//3), y+j*(n//3), n//3)
+
+sol(0,0,n)
 for i in range(n):
-  result=0
-  a=str(i)
-  for j in a:
-    result+=int(j)
-  result+=i
-  if result==n:
-    print(i)
-    break
-  elif result!=n and i==n-1:print(0)
+  arr[i].append("\n")
+result=""
+for i in range(n):
+  for j in range(n+1):
+    result+=arr[i][j]
+result=result[:-1]
+print(result)
