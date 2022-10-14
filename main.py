422572f7669acee52dc88d893e50aa9a580b12ff
GwanHwi1 <ghc98@naver.com>
2022-10-14 16:55:14 +0900

    1차원 정원

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 test_case=int(input())
 for t in range(1,test_case+1):
-  n=input()
-  total=[]
-  for i in range(2,10):
-    s=int(n)*i
-    total.append(sorted((str(s))))
-  if sorted(n) in total: print(f"#{t} possible")
-  else: print(f"#{t} impossible")
-  
+  n,d=map(int, input().split())
+  a=d*2+1
+  cnt=n//a
+  if n%a!=0: cnt+=1
+  print(f"#{t} {cnt}")
