91201dd9aac26ec5c95e4805e67403782989bab6
GwanHwi1 <ghc98@naver.com>
2022-10-17 18:19:47 +0900

    회문

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
-def RightCrossPlus(x,y):
-  global right
-  if x==100: return
-  right+=arr[x][y]
-  RightCrossPlus(x+1,y+1)
-
-def LeftCrossPlus(x,y):
-  global left
-  if x==100: return
-  left+=arr[x][y]
-  LeftCrossPlus(x+1,y-1)
-
 for t in range(1,11):
   n=int(input())
-  arr=[list(map(int,input().split()))for _ in range(100)]
-  right=0
-  left=0
-  result=0
-  total=0
-  RightCrossPlus(0,0)
-  LeftCrossPlus(0,99)
-  for i in range(100): #행
-    if result<sum(arr[i]): result=sum(arr[i])
-  for i in range(100): #열
-    for j in range(100):
-      total+=arr[j][i]
-    if result<total: result=total
-    total=0
-  result=max(left,right,result)
-  print(f"#{n} {result}")
+  lst=[list(map(str,input()))for _ in range(8)]
+  cnt=0
+  s=""
+  s1=""
+  for i in range(8):
+    for j in range(8-n+1):
+      s="".join(a for a in lst[i][j:j+n])
+      for k in range(j+n-1,j-1,-1):
+        s1+=lst[i][k]
+      if s==s1: cnt+=1
+      s=""
+      s1=""
+  for i in range(8):
+    for j in range(8-n+1):
+      for k in range(j,j+n):
+        s+=lst[k][i]
+      for k in range(j+n-1,j-1,-1):
+        s1+=lst[k][i]
+      if s==s1: cnt+=1
+      s=""
+      s1=""
+  print(f"#{t} {cnt}")
 
