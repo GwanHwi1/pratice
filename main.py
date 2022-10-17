001ec507028da47d323be434b9a0df0c02dfdaba
GwanHwi1 <ghc98@naver.com>
2022-10-17 15:23:50 +0900

    Nqueen

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-def flatten(n):
-  global result
-  boxes.sort()
-  result=boxes[-1]-boxes[0]
-  if result<=1 or n==0: return result
-  boxes[-1]-=1
-  boxes[0]+=1
-  flatten(n-1)
+def checking(a):
+  global cnt
+  if len(arr)==n: 
+    cnt+=1
+    return
+  for i in range(n):
+    if exclude(a,i):
+      arr.append([a,i])
+      checking(a+1)
+      arr.pop()
+      
+def exclude(x,y):
+  tf=True
+  for i in range(len(arr)):
+    if arr[i][0]==x or arr[i][1]==y or abs(x-arr[i][0])==abs(y-arr[i][1]):
+      tf=False
+      break
+  return tf
+    
+test_case=int(input())
+for t in range(1,test_case+1):
+  n=int(input())
+  cnt=0
+  arr=[]
+  checking(0)
+  print(f"#{t} {cnt}")
 
-for t in range(1,11):
-  a=int(input())
-  boxes=list(map(int,input().split()))
-  result=0
-  flatten(a)
-  print(f"#{t} {result}")
-
-  
