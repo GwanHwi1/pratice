3b6667786487d046a4bb679ef7a612d6846f781f
GwanHwi1 <ghc98@naver.com>
2022-10-17 21:40:18 +0900

    부분 수열의 합

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-for t in range(1,11):
-  n=int(input())
-  lst=[list(map(str,input()))for _ in range(8)]
+def dfs(goal,x):
+  global cnt
+  if goal==k:
+    cnt+=1
+    return
+  elif goal>k: return
+  for i in range(x,n):
+    dfs(goal+a[i],i+1)
+    
+test_case=int(input())
+for t in range(1,test_case+1):
+  n,k=map(int,input().split())
+  a=list(map(int,input().split()))
   cnt=0
-  s=""
-  s1=""
-  for i in range(8):
-    for j in range(8-n+1):
-      s="".join(a for a in lst[i][j:j+n])
-      for k in range(j+n-1,j-1,-1):
-        s1+=lst[i][k]
-      if s==s1: cnt+=1
-      s=""
-      s1=""
-  for i in range(8):
-    for j in range(8-n+1):
-      for k in range(j,j+n):
-        s+=lst[k][i]
-      for k in range(j+n-1,j-1,-1):
-        s1+=lst[k][i]
-      if s==s1: cnt+=1
-      s=""
-      s1=""
+  dfs(0,0)
   print(f"#{t} {cnt}")
-
