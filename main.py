9092ce839c579f340217a50d94b708ea5f3e4dab
GwanHwi1 <ghc98@naver.com>
2022-10-18 15:19:58 +0900

    최장 경로

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
+def dfs(position,a):
+  global result
+  tf=True
+  for i in lst[position]:
+    if not visited[i]:
+      tf=False
+      break
+  if tf:
+    result=max(result,a)
+    return
+  for i in lst[position]:
+    if not visited[i]:
+      visited[i]=True
+      dfs(i,a+1)
+      visited[i]=False
+
 test_case=int(input())
 for t in range(1,test_case+1):
-  n=input()
+  n,m=map(int, input().split())
+  lst=[[] for _ in range(n+1)]
+  for i in range(1,n+1):
+    lst[0].append(i)
+  visited=[False]*(n+1)
   result=0
-  tf=-1
-  for i in n:
-    if tf==-1 and i=="1":
-      result+=1
-      tf=1
-    if tf==1 and i=="0":
-      result+=1
-      tf=0
-    if tf==0 and i=="1":
-      result+=1
-      tf=1
+  for _ in range(m):
+    x,y=map(int, input().split())
+    lst[x].append(y)
+    lst[y].append(x)
+  # print(lst)
+  dfs(0,0)
   print(f"#{t} {result}")
--- practice.py
+++ practice.py
@@ -1,7 +1,3 @@
-a=[[1,2,3],[1,2,3],[1,2,3]]
-b=[[0]*3 for _ in range(3)]
-for i in range(3):
-  for j in range(3):
-    b[i][j]=a[j][i]
-
-print(b)
+a=[[1,2,3],[4,5,6]]
+for i in a[1]:
+  print(i)
