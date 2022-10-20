b12c0953dd6e5a1d215274cb39b65480672df3a8
GwanHwi1 <ghc98@naver.com>
2022-10-20 16:42:35 +0900

    String

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def square(a):
-  if a<=0: return 1
-  return n * square(a-1)
-
 for _ in range(1,11):
   t=int(input())
-  n,m=map(int,input().split())
-  print(f"#{t} {square(m)}")
+  check=input()
+  s=input()
+  cnt=0
+  for i in range(len(s)):
+    if s[i:i+len(check)]==check: cnt+=1
+  print(f"#{t} {cnt}")
