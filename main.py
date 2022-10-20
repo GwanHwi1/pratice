969b88bbce78eedf59246504e080133cfde12739
GwanHwi1 <ghc98@naver.com>
2022-10-20 14:28:31 +0900

    진기의 최고급 붕어빵

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-def dfs(x,y,now):
-  global result
-  if y>l: return
-  result=max(result,x)
-  for i in range(now,len(arr)):
-    dfs(x+arr[i][0],y+arr[i][1],i+1)
+def check(a):
+  global tf,k,arr
+  if k>=len(arr): k=len(arr)
+  for i in range(k):
+    if m*a>arr[i]: 
+      tf=False
+      break
+  if not tf or len(arr)==0: return
+  else: 
+    arr=arr[k:]
+    check(a+1)
 
 test_case=int(input())
 for t in range(1,test_case+1):
-  n,l=map(int,input().split())  
-  arr=[list(map(int,input().split())) for _ in range(n)]
-  result=0
-  dfs(0,0,0)
-  print(f"#{t} {result}")
+  n,m,k=map(int,input().split())
+  arr=list(map(int,input().split()))
+  arr.sort()
+  tf=True
+  check(1)
+  if tf: print(f"#{t} Possible")
+  else: print(f"#{t} Impossible")
